5f92252bc6329bee3dde555e7ff58ea7746e9e73
Megan Horsfield <meganthadev@gmail.com>
2020-07-05 02:45:27 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,10 +3,10 @@ module SongsHelper
   def display_artist(song)
     if song.artist
       link_to song.artist.name, song.artist
-    else 
-      render '/songs/edit'
-  end  
-end 
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
     
 
 end
